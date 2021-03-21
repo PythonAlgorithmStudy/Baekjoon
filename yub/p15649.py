4a825a6cafdd36c3d8a145623a968873287cb4f6
YubeenSon <s49451392@gmail.com>
2021-03-21 19:26:37 +0900

    0308 problem solving

---
+++ yub/p15649.py
@@ -0,0 +1,10 @@
+import sys
+import itertools  
+N, M = map(int, sys.stdin.readline().split())
+
+arr = list(range(N + 1))[1:]
+result = list(itertools.permutations(arr, M))
+for i in range(len(result)):
+    for j in range(M):
+        print(result[i][j], end=" ")
+    print(end="\n")
